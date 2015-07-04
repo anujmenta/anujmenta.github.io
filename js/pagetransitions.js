7167e5851debf0f8671e6352b54629fdf43e8480
Anuj Menta <anujmenta@gmail.com>
2015-07-04 13:10:14 +0530

    Hugerbox site

--- js/pagetransitions.js
+++ js/pagetransitions.js
@@ -1,133 +1,320 @@
-current=0;
-learnmore_count = 1;
-teampage_current = 0;
-learnmore_open = false;
+// $(document).ready(function(){
+ 
+//    $.ajax({
+//    	type:'GET',
+//     url: 'http://www.hungerbox.in/get_mobile.php',
+//     data: {'item':'menus','p':'8892824665'},
+//     dataType: 'json/application',
+//     success: function menupopulater(json){},
+// 	});
+
+// });
+
+hp_current = 1;
+
+order_cart = 0;
+
+area = '';
+
+order_cart_items = [];
+
+var cart = {};
+// order_quantity = 0;
+var json1 = {"num":2,"menus":[{"name":"Veg Meal","id":731,"items":"Mixed vegetables in sweet and sour gravy and dry chilli mushroom served with schezwan noodles","price":90,"discount":0,"vendor":"Swaad","quantity":9,"is_veg":1,"is_premium":0},{"name":"Non Veg Meal","id":732,"items":"NA","price":100,"discount":0,"vendor":"Swaad","quantity":10,"is_veg":0,"is_premium":0}],"dt":"Friday, 03-07-2015"};
+function menupopulater(json){
+	console.log('entered');
+	var num = json.num;
+	var menuitems = json.menus;
+	var master = document.getElementById('menu_list_wrapper');
+	for (var i=0; i<num; i++){
+		cart[menuitems[i].id] = 0;
+		var menucard = document.createElement('div');
+		menucard.className = 'menu_item';
+		var img = document.createElement('img');
+		var img_lcl = menuitems[i].name.toLowerCase().replace(/ /g, '_');
+
+		var div_img = document.createElement('div');
+		div_img.className = ('menu_item_image');
+		div_img.style.backgroundImage = 'url(http://www.hungerbox.in/img/'+img_lcl+'.jpg)';
+		menucard.appendChild(div_img);
+		// img.src = 'http://www.hungerbox.in/img/'+img_lcl+'.jpg';
+		// img.className = 'menu_item_image';
+		// menucard.appendChild(img);
+		
+		// var image_div = document.getElementsByClassName('menu_item_image');
+		var vegtoggle = document.createElement('img');
+		if (menuitems[i].is_veg==1){
+			console.log('Veg');
+			var vegtoggle_src = 'images/veg-icon.png';
+		}
+		else{
+			var vegtoggle_src = 'images/non-veg-icon.png';
+		}
+		vegtoggle.src = vegtoggle_src;
+		vegtoggle.className = 'menu_item_vegtoggle';
+		div_img.appendChild(vegtoggle);
+
+
+		var title = document.createElement('p');
+		title.className = 'menu_item_title';
+		title.innerHTML = menuitems[i].name+'<hr>';
+		
+		var price = document.createElement('p');
+		price.innerHTML = 'Rs. ' + menuitems[i].price;
+		price.className = 'menu_item_price';
+
+		var vendor = document.createElement('p');
+		vendor.className = 'menu_item_vendor'
+		vendor.innerHTML = menuitems[i].vendor;
+
+		var items = document.createElement('p');
+		items.className = 'menu_item_desc';
+		items.innerHTML = menuitems[i].items;
+
+		var counter = document.createElement('div');
+		counter.className = 'menu_item_counter';
+		// var minus = document.createElement('div');
+		// minus.setAttribute('id', 'menu_item_minus');
+		// minus.style.backgroundImage = 'url(images/minus.png)';
+		// // var min = document.createElement('p');
+		// // min.innerHTML = '-';
+		// // minus.appendChild(min);
+		// counter.appendChild(minus);
+
+		var minus = document.createElement('div');
+		minus.setAttribute('id', 'menu_item_minus_'+menuitems[i].id);
+		minus.style.display = 'inline-block';
+		minus.style.border = '3px solid';
+		minus.style.height = '100%';
+		minus.style.width = '12.5%';
+		minus.style.cursor = 'pointer';		
+		// $('#menu_item_plus').data('mid', { foo: 'bar' });
+		// minus.setAttribute('data-mid',menuitems[i].id);
+		minus.setAttribute('onclick', 'counter_minus(this.id)');
+		// plus.style.backgroundImage = 'url(images/plus.png)';
+		minus.innerHTML = '-';
+		counter.appendChild(minus);
+
+		var count = document.createElement('div');
+		count.setAttribute('id', 'menu_item_count_'+menuitems[i].id);
+		count.style.display = 'inline-block';
+		count.style.textAlign = 'center';
+		count.style.height = '100%';
+		count.style.width = '12.5%';
+		minus.setAttribute('data-mid',menuitems[i].id);
+		// var cnt = document.createElement('p');
+		// cnt.innerHTML = order_quantity;
+		// count.appendChild(cnt);
+		count.innerHTML = 0;
+		counter.appendChild(count);
+		// var min = document.createElement('p');
+		// min.innerHTML = '-';
+		// count.appendChild(min);
+
+		var plus = document.createElement('div');
+		plus.style.display = 'inline-block';
+		plus.style.border = '3px solid';
+		plus.style.height = '100%';
+		plus.style.width = '12.5%';
+		plus.style.cursor = 'pointer';
+		plus.setAttribute('id', 'menu_item_plus_'+menuitems[i].id);
+		// minus.setAttribute('data-mid',menuitems[i].id);
+		plus.setAttribute('onclick', 'counter_plus(this.id)');
+		// plus.style.backgroundImage = 'url(images/plus.png)';
+		plus.innerHTML = '+';
+		counter.appendChild(plus);
+
+		// var plus = document.createElement('p');
+		// plus.innerHTML = '-<span>   '+order_cart+'   </span>+';
+		// plus.className = 'menu_item_plus';
+
+		// var minus = document.createElement('p');
+		// minus.innerHTML = '-';
+		// minus.className = 'menu_item_plus';
+		
+
+		div_img.appendChild(price);
+		menucard.appendChild(title);
+		div_img.appendChild(vendor);
+		menucard.appendChild(items);
+		menucard.appendChild(counter);
+		// menucard.appendChild(plus);
+		// menucard.appendChild(minus);
+		master.appendChild(menucard);
+		console.log('Completed');
+	}
+
+}
+
+menupopulater(json1);
+
+function counter_plus(event1){
+	$('#checkout_button').fadeIn(500);
+	console.log(event1);
+	// $('#checkout_button').fadeIn(500);
+	var id = event1.substr(event1.length - 3);
+	var order_quantity = document.querySelector('#menu_item_count_'+id).innerHTML;
+	order_quantity++;
+	console.log(order_quantity);
+	document.querySelector('#menu_item_count_'+id).innerHTML = order_quantity;
+	order_cart_items.push(id);
+	cart[id] +=1;
+	document.querySelector('#carttext').innerHTML = 'Selected: '+order_cart_items.length+' Items';
+	// $('#menu_item_count').
+}
+
+function counter_minus(event1){
+	console.log(event1);
+	var id = event1.substr(event1.length - 3);
+	var order_quantity = document.querySelector('#menu_item_count_'+id).innerHTML;
+	if (order_quantity!=0){
+		order_quantity--;
+		console.log(order_quantity);
+		document.querySelector('#menu_item_count_'+id).innerHTML = order_quantity;
+		console.log(id);
+		var indexremove = order_cart_items.indexOf(id)
+		order_cart_items.splice(indexremove, 1);
+		cart[id] -=1;
+	document.querySelector('#carttext').innerHTML = 'Selected: '+order_cart_items.length+' Items';
+	}
+}
 function teampageswitcher(indiv){
-	$('#teampage_right_center').fadeOut(200);
-	$('#teampage_right_'+teampage_current).fadeOut(200);
-	$('#teampage_right_'+indiv).fadeIn(200);
-	teampage_current = indiv;
+	$('#'+hp_current).removeClass('test');
+	$('#'+indiv).addClass('test');
+	if (hp_current==indiv){
+		hp_current=0;
+		indiv=0;
+	}
+	// $('#teampage_right_center').fadeOut(700);
+	$('#homepage_right').css('background-image', 'url(images/background'+indiv+'.jpg)');
+	if (indiv==2){
+		$('#hp_right_1_mask').css('background-color', 'rgba(31, 129, 189, 0.85)');
+	}
+	if (indiv==1){
+		$('#hp_right_1_mask').css('background-color', 'rgba(237, 42, 38, 0.85)');
+	}
+	if (indiv==3){
+		$('#hp_right_1_mask').css('background-color', 'rgba(38, 237, 146, 0.85)');
+	}
+	if (indiv==1 || indiv==2 || indiv==3){
+		$('#homepage_left').css('background-color', '#F2F9BD');
+		$('#hp_right_1_mask').css('color', '#fff');
+		$('.codrops-top a').css('color', '#fff');
+	}
+	if (indiv==4){
+		$('#hp_right_1_mask').css('background-color', '#FFF');
+		$('#homepage_left').css('background-color', '#FFF');
+		$('#hp_right_1_mask').css('color', 'rgba(237, 42, 38, 0.85)');
+		$('.codrops-top a').css('color', 'rgba(237, 42, 38, 0.85)');
+	}
+	$('#hp_right_'+hp_current).fadeOut(50);
+	$('#hp_right_'+indiv).fadeIn(700);
+	hp_current = indiv;
 }
 
-function howitworks(){
+function ordernow(){
 	$main = $( '#pt-main' );
 	$pages = $main.children( 'div.pt-page' );
-	$currPage = $pages.eq(6);
+	$currPage = $pages.eq(1);
 	$currPage.addClass('pt-page-current pt-page-moveFromBottom');
-	$('#learnmore_bottom_1').css('animation', 'moveFromBottom 1s ease both');
 	learnmore_open=true;
+	$('#header').hide();
 }
 
-function howitworks_back(){
-	$currPage.removeClass('pt-page-current pt-page-moveFromBottom');
-	$currPage.addClass('pt-page-moveToBottom')
+function ordernow_back(){
+	$currPage.removeClass('pt-page-moveFromBottom');
+	$currPage.addClass('pt-page-moveToBottom');
+	$('#header').show();
 }
 
-function learnmore_previous(){
-	if (learnmore_count>1){
-		learnmore_previous_core();
-	}
-}
-
-function learnmore_next(){
-	if (learnmore_count<3){
-		learnmore_next_core();
-	}
-}
-function learnmore_previous_core(){
-	$('#learnmore_top_center_'+learnmore_count).fadeOut(500);
-	$('#learnmore_bottom_'+learnmore_count).css('animation', 'moveToBottom 1s ease both');
-	$('#learnmore_top_center_'+learnmore_count).hide();
-	$('#learnmore_bottom_'+learnmore_count).hide();
-	learnmore_count--;
-	$('#learnmore_top_center_'+learnmore_count).show();
-	$('#learnmore_bottom_'+learnmore_count).show();
-	$('#learnmore_top_center_'+learnmore_count).fadeIn(600);
-	$('#learnmore_bottom_'+learnmore_count).css('animation', 'moveFromBottom 1s ease both');
-}
-
-function learnmore_next_core(){
-	$('#learnmore_bottom_'+learnmore_count).css('animation', 'moveToBottom 1s ease both');
-	$('#learnmore_top_center_'+learnmore_count).fadeOut(500);
-	$('#learnmore_bottom_'+learnmore_count).hide();
-	$('#learnmore_top_center_'+learnmore_count).hide();
-	learnmore_count++;
-	$('#learnmore_top_center_'+learnmore_count).show();
-	$('#learnmore_bottom_'+learnmore_count).show();
-	$('#learnmore_top_center_'+learnmore_count).fadeIn(600);
-	$('#learnmore_bottom_'+learnmore_count).css('animation', 'moveFromBottom 1s ease both');
-
-}
-function clickbutton(a){
-	// if (learnmore_open){
-	// 	howitworks_back();
-	// }
-	$('#'+current).removeClass('test');
-	$('#'+a).addClass('test');
+function ordernext(areaid){
+	area = document.getElementById(areaid).innerHTML;
 	$main = $( '#pt-main' );
-	inClass = 'pt-page-moveFromRight'
-	outClass = 'pt-page-moveToLeft'
-	inClassRev = 'pt-page-moveFromLeft'
-	outClassRev = 'pt-page-moveToRight'
-	presentClass = 'pt-page-current'
-	isAnimating = false,
-	endCurrPage = false,
-	endNextPage = false,
-	animEndEventNames = {
-			'WebkitAnimation' : 'webkitAnimationEnd',
-			'OAnimation' : 'oAnimationEnd',
-			'msAnimation' : 'MSAnimationEnd',
-			'animation' : 'animationend'
-		}
-	animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ]
-	support = Modernizr.cssanimations;
 	$pages = $main.children( 'div.pt-page' );
-	$currPage = $pages.eq( current );
-	// $currPage.removeClass(inClass, outClass);
-	// console.log($currPage);
-	// $currPage.addClass(outClass);
-	// if (current>a) {console.log('Detected')};
-	// $nextPage.addClass(inClass);
-	if (current>a){console.log('ldld');inClass=inClassRev;outClass=outClassRev};
-	current=a;
-	$nextPage = $pages.eq(current).addClass('pt-page-current');
-	$currPage.addClass( outClass ).on( animEndEventName, function() {
-		$currPage.off( animEndEventName );
-		endCurrPage = true;
-		if( endNextPage ) {
-			onEndAnimation( $currPage, $nextPage );
-		}
-	} );
-	
-	$nextPage.addClass( inClass ).on( animEndEventName, function() {
-		$nextPage.off( animEndEventName );
-		endNextPage = true;
-		if( endCurrPage ) {
-			onEndAnimation( $currPage, $nextPage );
-		}
-	} );
-
-	if( !support ) {
-		onEndAnimation( $currPage, $nextPage );
-	}
-	function onEndAnimation( $outpage, $inpage ) {
-		endCurrPage = false;
-		endNextPage = false;
-		resetPage( $outpage, $inpage );
-		isAnimating = false;
-	}
-
-	function resetPage( $outpage, $inpage ) {
-		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
-		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
-	}
-
+	$currPage = $pages.eq(1);
+	$nextPage = $pages.eq(2);
+	$nextPage.addClass('pt-page-current');
+	// $currPage.removeclass('pt-page-current pt-page-moveFromBottom');
+	$nextPage.addClass('pt-page-moveFromRight');
+	// $('#checkout_button').delay(1000).css('animation', 'moveFromTop .6s ease both');
 }
-$('.howitworks').click(function(){
-	var randomnumber = Math.floor(Math.random() * (5)) + 1;
-	clickbutton(randomnumber);	
-});
 
+function ordernext_back(){
+	$nextPage.removeClass('pt-page-moveFromRight');
+	$nextPage.addClass('pt-page-moveToRight');
+	// $currPage.removeClass('pt-page-moveFromRight');
+	// $currPage.addClass('pt-page-moveToRight');
+}
+
+function ordersummary(){
+	$main = $( '#pt-main' );
+	$pages = $main.children( 'div.pt-page' );
+	$currPage = $pages.eq(2);
+	$nextPage = $pages.eq(3);
+	$nextPage.addClass('pt-page-current');
+	document.getElementById('Area').placeholder = area;
+	// $currPage.removeclass('pt-page-current pt-page-moveFromBottom');
+	$nextPage.addClass('pt-page-moveFromRight');
+	ordersummary_tablepopulate();
+}
+
+function returnname(id_use){
+	for(var i = 0; i < json1.menus.length; i++)
+	{
+	  if(json1.menus[i].id == id_use)
+	  {
+	    return json1.menus[i].name;
+	  }
+	}
+}
+
+function returnprice(id_use){
+	for(var i = 0; i < json1.menus.length; i++)
+	{
+	  if(json1.menus[i].id == id_use)
+	  {
+	    return json1.menus[i].price;
+	  }
+	}
+}
+
+function returntotalamount(){
+	var total = 0;
+	for (var index in cart){
+		total+= cart[index]*returnprice(index);
+	}
+	return total;
+}
+
+function ordersummary_tablepopulate(){
+	$('#ordersummary_right_wrapper').empty();
+	len = order_cart_items.length;
+	var table = document.getElementById('ordersummary_right_wrapper');
+	for (var index in cart){
+		if (cart[index]!=0){
+			var p_left = document.createElement('p');
+			p_left.innerHTML = returnname(index)+' X '+cart[index]+'<br>';
+			p_left.className = 'ordersummary_table_left';
+			table.appendChild(p_left);
+
+			var p_right = document.createElement('p');
+			p_right.innerHTML = 'Rs. ' + cart[index]*returnprice(index);
+			p_right.className = 'ordersummary_table_right';
+			table.appendChild(p_right);
+			var br = document.createElement('br');
+			table.appendChild(br);
+		}
+	}
+	var total = document.createElement('h2');
+	total.innerHTML = 'Total Amount:  Rs'+returntotalamount();
+	total.className = 'ordersummary_table_total';
+	table.appendChild(total);
+}
+
+function ordersummary_back(){
+	$nextPage.removeClass('pt-page-moveFromRight');
+	$nextPage.addClass('pt-page-moveToRight');
+}
 
 var PageTransitions = (function() {
 
@@ -198,8 +385,9 @@ var PageTransitions = (function() {
 			current = 0;
 		}
 
-		var $nextPage = $pages.eq( animation-1 ).addClass( 'pt-page-current' ),
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
 			outClass = '', inClass = '';
+
 		switch( animation ) {
 
 			case 1:
@@ -207,16 +395,16 @@ var PageTransitions = (function() {
 				inClass = 'pt-page-moveFromRight';
 				break;
 			case 2:
-				outClass = 'pt-page-moveToLeft';
-				inClass = 'pt-page-moveFromRight';
+				outClass = 'pt-page-moveToRight';
+				inClass = 'pt-page-moveFromLeft';
 				break;
 			case 3:
-				outClass = 'pt-page-moveToLeft';
-				inClass = 'pt-page-moveFromRight';
+				outClass = 'pt-page-moveToTop';
+				inClass = 'pt-page-moveFromBottom';
 				break;
 			case 4:
-				outClass = 'pt-page-moveToLeft';
-				inClass = 'pt-page-moveFromRight';
+				outClass = 'pt-page-moveToBottom';
+				inClass = 'pt-page-moveFromTop';
 				break;
 			case 5:
 				outClass = 'pt-page-fade';
